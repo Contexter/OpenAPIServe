8e6405a484120343b2dc48d424020a6b2952dc88
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-21 15:28:32 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0")
     ],
     targets: [
         // Main OpenAPIServe target
@@ -23,33 +22,11 @@ let package = Package(
             name: "OpenAPIServe",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "Leaf", package: "leaf"),
-                .product(name: "Algorithms", package: "swift-algorithms")
+                .product(name: "Leaf", package: "leaf")
             ],
             path: "Sources/OpenAPIServe",
             resources: [
                 .process("Resources")
-            ]
-        ),
-        
-        // Utilities for testing
-        .target(
-            name: "Utilities",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                "OpenAPIServe"
-            ],
-            path: "Tests/OpenAPIServeTests/Utilities"
-        ),
-
-        // Test target
-        .testTarget(
-            name: "OpenAPIServeTests",
-            dependencies: ["OpenAPIServe", "Utilities"],
-            path: "Tests/OpenAPIServeTests",
-            exclude: ["Utilities"],
-            resources: [
-                .process("Resources/Views")
             ]
         )
     ]
